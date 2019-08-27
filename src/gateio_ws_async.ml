decdeba85909255c24f852d480ded6dbd191a3c7
Vincent Bernardoff <vb@luminar.eu.org>
2019-08-27 17:31:21 +0200

    async: add auto ping

--- src/gateio_ws_async.ml
+++ src/gateio_ws_async.ml
@@ -8,46 +8,70 @@ let url = Uri.make ~scheme:"https" ~host:"ws.gateio.ws" ~path:"v3/" ()
 
 let src = Logs.Src.create "gateio.ws.async"
 module Log = (val Logs.src_log src : Logs.LOG)
+module Log_async = (val Logs_async.src_log src : Logs_async.LOG)
 
-let connect () =
+let string_of_json encoding cmd =
+  match Ezjsonm_encoding.construct encoding cmd with
+  | `A _ | `O _ as a -> Ezjsonm.to_string a
+  | _ -> invalid_arg "not a json document"
+
+let incr64 r = r := Int64.succ !r
+
+let connect ?ping () =
   Fastws_async.connect_ez url >>|
   Result.map ~f:begin fun (r, w, cleaned_up) ->
+    let pingid = ref Int64.(100_000L + (Random.int64 100_000L)) in
     let client_read = Pipe.map r ~f:begin fun msg ->
         Ezjsonm_encoding.destruct_safe encoding (Ezjsonm.from_string msg)
       end in
     let ws_read, client_write = Pipe.create () in
     don't_wait_for
       (Pipe.closed client_write >>| fun () -> Pipe.close w) ;
+    begin match ping with
+      | None -> ()
+      | Some span ->
+        Clock_ns.every' span ~stop:(Pipe.closed w) begin fun () ->
+          let ping = string_of_json encoding (Ping !pingid) in
+          incr64 pingid ;
+          Log_async.debug (fun m -> m "-> %s" ping) >>= fun () ->
+          Pipe.write w ping
+        end
+    end ;
     don't_wait_for @@
     Pipe.transfer ws_read w ~f:begin fun cmd ->
-      let doc =
-        match Ezjsonm_encoding.construct encoding cmd with
-        | `A _ | `O _ as a -> Ezjsonm.to_string a
-        | _ -> invalid_arg "not a json document" in
+      let doc = string_of_json encoding cmd in
       Log.debug (fun m -> m "-> %s" doc) ;
       doc
     end ;
     (client_read, client_write, cleaned_up)
   end
 
-let connect_exn () =
-  connect () >>= function
+let connect_exn ?ping () =
+  connect ?ping () >>= function
   | Error `Internal exn -> raise exn
   | Error `WS e -> Fastws_async.raise_error e
   | Ok a -> return a
 
-let with_connection f =
+let with_connection ?ping f =
   Fastws_async.with_connection_ez url ~f:begin fun r w ->
+    let pingid = ref Int64.(100_000L + (Random.int64 100_000L)) in
+    begin match ping with
+      | None -> () ;
+      | Some span ->
+        Clock_ns.every' span ~stop:(Pipe.closed w) begin fun () ->
+          let ping = string_of_json encoding (Ping !pingid) in
+          incr64 pingid ;
+          Log_async.debug (fun m -> m "-> %s" ping) >>= fun () ->
+          Pipe.write w ping
+        end
+    end ;
     let r = Pipe.map r ~f:begin fun msg ->
         Ezjsonm_encoding.destruct_safe encoding (Ezjsonm.from_string msg)
       end in
     let ws_read, client_write = Pipe.create () in
     don't_wait_for @@
     Pipe.transfer ws_read w ~f:begin fun cmd ->
-      let doc =
-        match Ezjsonm_encoding.construct encoding cmd with
-        | `A _ | `O _ as a -> Ezjsonm.to_string a
-        | _ -> invalid_arg "not a json document" in
+      let doc = string_of_json encoding cmd in
       Log.debug (fun m -> m "-> %s" doc) ;
       doc
     end ;
@@ -56,8 +80,8 @@ let with_connection f =
       ~finally:(fun () -> Pipe.close_read ws_read ; Deferred.unit)
   end
 
-let with_connection_exn f =
-  with_connection f >>= function
+let with_connection_exn ?ping f =
+  with_connection ?ping f >>= function
   | Error `Internal exn -> raise exn
   | Error `User_callback exn -> raise exn
   | Error `WS e -> Fastws_async.raise_error e
--- src/gateio_ws_async.mli
+++ src/gateio_ws_async.mli
@@ -1,18 +1,21 @@
+open Core
 open Async
 
 open Gateio_ws
 
-val connect : unit ->
+val connect : ?ping:Time_ns.Span.t -> unit ->
   (t Pipe.Reader.t * t Pipe.Writer.t * unit Deferred.t,
    [ `Internal of exn | `WS of Fastws_async.error ]) result Deferred.t
 
-val connect_exn : unit ->
+val connect_exn : ?ping:Time_ns.Span.t -> unit ->
   (t Pipe.Reader.t * t Pipe.Writer.t * unit Deferred.t) Deferred.t
 
 val with_connection :
+  ?ping:Time_ns.Span.t ->
   (t Pipe.Reader.t -> t Pipe.Writer.t -> 'a Deferred.t) ->
   ('a, [ `Internal of exn
        | `User_callback of exn | `WS of Fastws_async.error ]) result Deferred.t
 
 val with_connection_exn :
+  ?ping:Time_ns.Span.t ->
   (t Pipe.Reader.t -> t Pipe.Writer.t -> 'a Deferred.t) -> 'a Deferred.t
