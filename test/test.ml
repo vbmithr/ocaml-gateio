b1852c71702e9a57234cfa478c69a0b2e5745018
Vincent Bernardoff <vb@luminar.eu.org>
2020-01-17 12:17:38 +0100

    test: use envvar instead of virtu file for credentials

--- test/test.ml
+++ test/test.ml
@@ -3,30 +3,15 @@ open Async
 
 open Gateio_rest
 
-module Cfg = struct
-  type cfg = {
-    key: string ;
-    secret: string ;
-    passphrase: string [@default ""];
-    quote: (string * int) list [@default []];
-  } [@@deriving sexp]
-
-  type t = (string * cfg) list [@@deriving sexp]
-end
-
-let default_cfg = Filename.concat (Option.value_exn (Sys.getenv "HOME")) ".virtu"
-let cfg =
-  List.Assoc.find_exn ~equal:String.equal
-    (Sexplib.Sexp.load_sexp_conv_exn default_cfg Cfg.t_of_sexp) "GATEIO"
-
-let () =
-  Logs.set_reporter (Logs_async_reporter.reporter ()) ;
-  Logs.set_level (Some Debug)
+let key, secret =
+  match String.split ~on:':' (Sys.getenv_exn "TOKEN_GATEIO") with
+  | [key; secret] -> key, secret
+  | _ -> assert false
 
 let wrap_request ?(speed=`Quick) n service =
   let auth = {
-    Fastrest.key = cfg.Cfg.key ;
-    secret = cfg.Cfg.secret ;
+    Fastrest.key = key ;
+    secret = secret ;
     meta = [] ;
   } in
   Alcotest_async.test_case n speed begin fun () ->
@@ -46,6 +31,8 @@ let rest = [
 ]
 
 let () =
+  Logs.set_reporter (Logs_async_reporter.reporter ()) ;
+  Logs.set_level (Some Debug) ;
   Alcotest.run "gateio" [
     "rest", rest ;
   ]
